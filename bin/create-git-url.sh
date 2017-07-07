7f01bd264020eff6d76202fbdd9e3daa36fe726b
Eike Hirsch <eike.hirsch@smartsteuer.de>
2017-07-07 13:40:51 +0200

    converted tabs to spaces.

--- bin/create-git-url.sh
+++ bin/create-git-url.sh
@@ -5,18 +5,18 @@ create_git_url () {
     credentials=$2
     protocol="https"
 
-	# The protocol is optional. But when one was provided we have to respect it.
-	# Otherwise we will fall back to https.
-	if [[ ${repo} =~ ^https?:// ]]; then
-		protocol=${repo%%:\/\/*}
-		repo=${repo/#http*:\/\//}
-	fi
+    # The protocol is optional. But when one was provided we have to respect it.
+    # Otherwise we will fall back to https.
+    if [[ ${repo} =~ ^https?:// ]]; then
+        protocol=${repo%%:\/\/*}
+        repo=${repo/#http*:\/\//}
+    fi
 
     if [ ! -z ${credentials} ]; then
-	    GIT_URL=${protocol}://${credentials}@${repo}
-	else
-	    echo "Assuming public repository, GIT_CREDENTIALS is empty"
-	    GIT_URL=${protocol}://${repo}
-	fi
+        GIT_URL=${protocol}://${credentials}@${repo}
+    else
+        echo "Assuming public repository, GIT_CREDENTIALS is empty"
+        GIT_URL=${protocol}://${repo}
+    fi
 
 }
