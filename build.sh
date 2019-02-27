4ee4f3d0a0d0f8174a2f10d7567f34ef1d642dd1
Danielsson <martin.danielsson@haufe-lexware.com>
2019-02-27 09:27:07 +0100

    WTF

--- build.sh
+++ build.sh
@@ -31,6 +31,7 @@ echo "Building normal image..."
 echo "============================================"
 
 export BUILD_ALPINE=""
+ls -la
 perl -pe 's;(\\*)(\$([a-zA-Z_][a-zA-Z_0-9]*)|\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})?;substr($1,0,int(length($1)/2)).($2&&length($1)%2?$2:$ENV{$3||$4});eg' Dockerfile.template > Dockerfile
 normalImageName="${DOCKER_PREFIX}api:${DOCKER_TAG}"
 if [[ "dev" = "${DOCKER_TAG}" ]]; then
