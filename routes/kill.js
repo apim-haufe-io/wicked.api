6529d64bfc57102ac5bc09c9327c5150e1412311
Maksim Likharev <maksim.likharev@thomsonreuters.com>
2019-07-29 15:47:38 -0700

    Sync 07 29 2019 (#26)
    * adding group for the subscription/approval events (#22)
    
    * Bump to version 1.0.0-rc.5
    
    * Fixes Haufe-Lexware/wicked.haufe.io#196
    
    * Fixes Haufe-Lexware/wicked.haufe.io#198
    
    * Bump to version 1.0.0-rc.6
    
    * Update docker group to adapt to new Jenkins
    
    * Use classical pipeline again (test)
    
    * Try this on the RD jenkins
    
    * Allow loading of javascript file in static content (#24)
    
    * Change back to "docker" agent
    
    * Take out SonarQube for the time being
    
    * Log in once to make sure docker login is done
    
    * Bump to version 1.0.0-rc.7
    
    * Enable switching off health checks
    
    * Bump to version 1.0.0-rc.8
    
    * Infinite loop inside docker container...
    ... instead of letting e.g. Kubernetes handle the restart. Advantages:
    - Much quicker restart
    - Kubernetes does not count a reload as a full crash
    
    See Haufe-Lexware/wicked.haufe.io#212
    
    * honor host (#25)
    
    * Partly fixes Haufe-Lexware/wicked.haufe.io#216

--- routes/kill.js
+++ routes/kill.js
@@ -1,10 +1,21 @@
 'use strict';
 
+/* global __dirname */
+
+const path = require('path');
+const fs = require('fs');
+
 const utils = require('./utils');
 const users = require('./users');
 
 const { debug, info, warn, error } = require('portal-env').Logger('portal-api:kill');
 const kill = require('express').Router();
+
+const KILL_SEMAPHORE = path.join(__dirname, '..', 'RELOAD_REQUESTED');
+// Sanity check at startup:
+if (fs.existsSync(KILL_SEMAPHORE)) {
+    fs.unlinkSync(KILL_SEMAPHORE);
+}
 
 const verifyKillScope = utils.verifyScope('restart_api');
 
@@ -30,6 +41,7 @@ kill.killApi = function (app, res, loggedInUserId) {
             return utils.fail(res, 403, 'Not allowed. This is admin land.');
         }
         warn('RESTARTING API DUE TO USER REQUEST');
+        fs.writeFileSync(KILL_SEMAPHORE, 'reload requested by API interaction.', 'utf8');
         res.status(204).json({});
         setTimeout(function () {
             process.exit(0);
