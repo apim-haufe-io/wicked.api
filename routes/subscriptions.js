ada99e3722a021dacb4f83ac1c6ddbb31c98ea28
vitaly_belmach <vitalii.belmach@clarivate.com>
2019-10-07 21:23:57 +0300

    Added application description field to subscribtions webhook (#30)
    * Added application description field to subscribtions webhook
    
    * Added migration script for db. Changed version to 4

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -411,6 +411,7 @@ subscriptions.addSubscription = function (app, res, applications, loggedInUserId
                             application: {
                                 id: appInfo.id,
                                 name: appInfo.name,
+                                description: appInfo.description,
                                 trusted: wantsTrusted
                             },
                             plan: {
